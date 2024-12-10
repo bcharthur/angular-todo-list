8f2e6aab61c98a18fa3e4a3a114b0b487690b405
abouchaud2023 <art.bouchaud@gmail.com>
2024-12-10 12:08:09 +0100

    ajout nav bar et système d'identification

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; // Importer RouterModule
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule], // Ajouter RouterModule ici
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  error: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.error = err.error.error || 'Erreur lors de la connexion';
+      },
+    });
+  }
+}
